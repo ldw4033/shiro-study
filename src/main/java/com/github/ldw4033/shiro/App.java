b4c26b0b9547cac520be072f4fc8509e9a1dbad0
liudw <ldw4033@163.com>
2015-01-20 21:05:09 +0800

    init

---
+++ src/main/java/com/github/ldw4033/shiro/App.java
@@ -0,0 +1,13 @@
+package com.github.ldw4033.shiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
